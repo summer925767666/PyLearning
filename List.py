7b9239c374b0e31c129f486872ae31a0cf1ec354
ChrisZhang <you@example.com>
2019-04-19 12:49:28 +0800

    添加列表的常用函数

--- List.py
+++ List.py
@@ -4,15 +4,49 @@
 字符串是静态的，列表是动态的
 """
 
-lst = [5.4, "hello", 2]  # 使用[]创建并初始化
-print(lst[:])
 
-print(lst[0])  # 使用[index]取值
+def list():
+    lst = [5.4, "hello", 2]  # 使用[]创建并初始化,类型可以不同！！！
 
-lst[0] = "chris"  # 可直接赋值
-print(lst[:])
+    print(lst[0])  # 使用[index]取值
+    print(lst[-1])
 
-lst[1:3] = ["hello", "world"]  # 使用切片的方式赋值
+    print(lst + ["chris"])  # 可以使用+拼接，不改变lst本身
 
-print(lst[:])
+    print(lst * 2)  # 重复
 
+    lst[0] = "chris"  # 可直接赋值
+
+    lst[1:3] = ["hello", "world"]  # 使用切片的方式赋值
+
+    lst.append("chris")  # 追加一个元素
+    lst.extend([1, 2, 3.4])  # 追加一个列表
+    lst.insert(0, 100)  # 插入
+
+    lst.pop()  # 删除最后一个元素
+    lst.remove("chris")  # 删除指定元素
+    lst.remove("hello")
+    lst.remove("world")
+    lst.remove("chris")
+
+    lst.sort()  # py3中不支持str和int的排序
+    lst.reverse()  # 逆序
+
+    print(lst)
+
+
+def main():
+    # 列表功能演示
+    # list()
+
+    # 求平均数
+    nums = []
+
+    for i in range(0, 5):
+        num = (float)(input())
+        nums.append(num)
+
+    print("the average is", sum(nums) / len(nums))
+
+
+main()
